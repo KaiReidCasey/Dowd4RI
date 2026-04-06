ea15a3bc14985356452e998fd5b312f4a8c5f827
Kai <9575844+KaiReidCasey@users.noreply.github.com>
2026-04-06 16:55:12 -0400

    Added basic routing support & hello world

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+// import LandingMainBody from '../components/LandingMainBody';
+// import TopBar from '../components/TopBar';
+// import Footer from '../components/Footer';
+
+export default function Home () {
+    return (
+        <div>
+            Hello World!
+            {/* <TopBar />
+            <LandingMainBody />
+            <Footer /> */}
+        </div>
+    );
+}
